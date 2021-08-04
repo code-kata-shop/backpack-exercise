db11975824dcf6b09741a014f61988fb6f9dc88d
Robert Roskam <raiderrobert@gmail.com>
2021-08-04 10:20:35 -0400

    Added another test

--- tests.py
+++ tests.py
@@ -6,19 +6,19 @@ from main import Backpack
 class BackpackTest(unittest.TestCase):
 
     def test_list_contents(self):
-        self.assertEquals(Backpack().get_contents(), [])
+        self.assertEqual(Backpack().get_contents(), [])
 
     def test_add_item(self):
         b = Backpack()
         b.add('ax')
-        self.assertEquals(b.get_contents(), ['ax'])
+        self.assertEqual(b.get_contents(), ['ax'])
 
     def test_remove_item(self):
         b = Backpack()
         b.add('ax')
-        self.assertEquals(b.get_contents(), ['ax'])
+        self.assertEqual(b.get_contents(), ['ax'])
         b.remove('ax')
-        self.assertEquals(b.get_contents(), [])
+        self.assertEqual(b.get_contents(), [])
 
     def test_multiple_alphabetical(self):
         b = Backpack()
@@ -26,7 +26,7 @@ class BackpackTest(unittest.TestCase):
         b.add('stick')
         b.add('Food')
         b.add('food')
-        self.assertEquals(b.get_contents(), ['ax', 'Food', 'food', 'stick'])
+        self.assertEqual(b.get_contents(), ['ax', 'Food', 'food', 'stick'])
 
     def test_get_counters(self):
         b = Backpack()
@@ -36,7 +36,7 @@ class BackpackTest(unittest.TestCase):
         b.add('food')
         b.add('food')
         b.add('ax')
-        self.assertEquals(b.get_counts(), {'ax': 2, 'stick': 1, 'Food': 1, 'food': 2})
+        self.assertEqual(b.get_counts(), {'ax': 2, 'stick': 1, 'Food': 1, 'food': 2})
 
     def test_most_popular(self):
         b = Backpack()
@@ -45,7 +45,18 @@ class BackpackTest(unittest.TestCase):
         b.add('Food')
         b.add('food')
         b.add('food')
-        self.assertEquals(b.most_popular(), {'food': 2})
+        self.assertEqual(b.most_popular(), {'food': 2})
+
+    def test_at_size(self):
+        b = Backpack(size=5)
+        b.add('ax')
+        b.add('stick')
+        b.add('Food')
+        b.add('food')
+        b.add('food')
+        b.add('food')
+        self.assertEqual(b.most_popular(), {'food': 3})
+        self.assertEqual(b.get_counts(), {'stick': 1, 'Food': 1, 'food': 3})
 
 
 if __name__ == '__main__':
