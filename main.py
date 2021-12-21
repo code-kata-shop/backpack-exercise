b85ecb5b4c30d1c0a7d26f9383228c30f6ca42c7
Robert Roskam <raiderrobert@gmail.com>
2021-12-21 10:11:16 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
-
-
 class Backpack:
-    pass
 
+    def __init__(self, size):
+        pass
+
+    def get_contents(self):
+        pass
+
+    def get_counts(self):
+        pass
+
+    def add(self, item):
+        pass
+
+    def remove(self, item):
+        pass
+
+    def most_popular(self):
+        pass
