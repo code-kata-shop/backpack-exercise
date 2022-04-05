e3c8e7912bed4b2594048b1421225bdd519ec36a
Robert Roskam <raiderrobert@gmail.com>
2022-04-05 15:30:39 -0400

    Update and rename hello.go to golang/hello.go

---
+++ golang/hello.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+
+type backpack struct {
+    size    int
+}
+
+
+func (b backpack) get_contents() {
+
+}
+
+func (b backpack) get_counts() {
+
+}
+
+func (b backpack) add(item string) {
+
+}
+
+func (b backpack) remove(item string) {
+
+}
+
+func (b backpack) most_popular() {
+	
+}
+
+
+func main() {
+	fmt.Println("Hello backpack 123.")
+}
